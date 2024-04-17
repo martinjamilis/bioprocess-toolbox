8d57afed336ab78aae3663d654c1d565d4daefba
martinjamilis <jamilismartin@gmail.com>
2024-04-17 13:55:28 -0300

    Se agregó el método Euler
    Se agregó el método Euler que permite simular el proceso con el método de Euler (paso 1, rectangular). 
    El método rates no admite arrays como entrada, hay que ver eso.

--- BioProcess.m
+++ BioProcess.m
@@ -1,16 +1,21 @@
 classdef BioProcess < dynamicprops 
-    %UNTITLED Summary of this class goes here
-    %   Detailed explanation goes here
+    % Bioprocess model class
+    %   Class to define bioprocess dynamic model properties and perform
+    %   calculations.
+    %   Instances must be created specifying number of states and
+    %   reactions.
     
     properties
         NumberStates {mustBeNumeric}
         NumberReactions {mustBeNumeric}
         YieldMatrix {mustBeNumeric}
         RateArray cell
+        Names cell
     end
     
     methods
         function obj=BioProcess(numStates,numReactions)  
+            % Constructor
             obj.NumberStates=numStates;
             obj.NumberReactions=numReactions;
             obj.YieldMatrix=nan(numStates,numReactions);
@@ -21,31 +26,77 @@ classdef BioProcess < dynamicprops
                 end
             end
             obj.RateArray = C;
+%             obj.Names= cell(1,numStates);
         end
         
-        function obj = set.YieldMatrix(obj,value)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            obj.YieldMatrix=value;           
+        function set.YieldMatrix(obj,value)
+            %set YieldMatrix Property. 
+            % Matrix must be NumberStates x NumberReactions
+            if size(value)== [obj.NumberStates,obj.NumberReactions]
+                obj.YieldMatrix=value;           
+            else
+                error('Incorrect dimensions, matrix must be %d by %d',obj.NumberStates,obj.NumberReactions);
+            end
         end
         
-        function obj = set.RateArray(obj,value)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here 
-            obj.RateArray=value; 
-        end
+        function set.RateArray(obj,value)
+            % set the kinetic rate factors
+            % the cell array RateArray contains the kinetic models factors 
+            % for each reaction rate with respect to each state variable.
+            % Each element of the aaray must belong to the kineticModel
+            % class
+            if iscell(value)
+                dim=size(value);
+                errores=0;
+                for idx=1:dim(1)*dim(2)
+                    if ~isequal(class(value{idx}),'kineticModel')                     
+                       errores=errores+1;
+                    end
+                end
+                if errores~=0
+                    error('One or more inserted elements do not belong to the class kineticModel'); 
+                else
+                    obj.RateArray=value; 
+                end
+            else
+                if isequal(class(value),'kineticModel')
+                    obj.RateArray=value; 
+                else
+                    error('The inserted element does not belong to the class kineticModel');                  
+                end
+            end
+        end     
         
-        function obj = get(inputArg1,inputArg2)
-            %UNTITLED Construct an instance of this class
-            %   Detailed explanation goes here
-            obj.Property1 = inputArg1 + inputArg2;
-        end
-        
-        function r = reactionRates(obj,xi)
-            r=nan(obj.NumberReactions,1);
+        function set.Names(obj,value)
+            % set the names of the state variables
+            % the names are stored in a cell array
+            % must be strings
+            if iscell(value)
+                if size(value)==[1,obj.NumberStates]
+                    errores=0;
+                    for idx=1:obj.NumberStates
+                        if ~isstring(value{idx}) && ~ischar(value{idx})                       
+                           errores=errores+1;
+                        end
+                    end
+                    if errores~=0
+                        error('One or more inserted elements is not a string'); 
+                    else
+                        obj.Names=value; 
+                    end
+                else
+                    error('The size of the cells do not match. Must have %d names',obj.NumberStates);
+                end
+            else
+                if ~isstring(value{idx}) || ~ischar(value{idx})
+                    error('The inserted elementis not a char or string'); 
+                else
+                    obj.Names=value; 
+                end
+            end
             
         end
-        
+       
         function dxidt = stateSpace(obj,t,xi,xi_in,D)
             %METHOD1 Summary of this method goes here
             %   Detailed explanation goes here
@@ -57,10 +108,23 @@ classdef BioProcess < dynamicprops
             for idx=1:obj.NumberReactions
                 for jdx=1:obj.NumberStates
                     modelo = obj.RateArray{idx,jdx};
-                    r(idx)= r(idx)*modelo.rate(xi(jdx));
+                    r(idx)= r(idx).*modelo.rate(xi(jdx));
                 end
             end
         end
+        
+        function [t,xi] = euler(obj,dt,tspan,xi0,xi_in,D)
+            t=tspan(1):dt:tspan(2);
+            N=length(t);
+            xi=nan(obj.NumberStates,N);
+            xi(:,1)=xi0;
+            for idx=2:N
+                xi(:,idx)=xi(:,idx-1)+dt*obj.stateSpace(t,xi(:,idx-1),xi_in,D);
+            end
+            t=t';
+            xi=xi';
+        end
+        
         
     end
 end
--- example.m
+++ example.m
@@ -1,14 +1,43 @@
 clear, clc, close all
 
-a=BioProcess(3,2)
+% s->x+p
+a=BioProcess(3,1)
+a.Names={'x', 's', 'p'};
 a.RateArray{1,1}=kineticModel('proportional',1);
-a.RateArray{2,1}=kineticModel('proportional',1);
 a.RateArray{1,2}=kineticModel('monod',0.5,0.5);
-a.RateArray{1,3}=kineticModel('inhibit',1,10);
-a.RateArray{2,2}=kineticModel('haldane',0.7,0.6,5);
+a.YieldMatrix = [1;-1/0.5;0.5];
+xi_in=[0;10;0];
+
+% s=linspace(0,10,100);
+% for idx=1:100
+%     mu(idx)=a.rates([1;s(idx)]);
+% end
+% figure(1); plot(s,mu)
+
+[t,xi]=a.euler(0.001,[0 48],[0.1;20;0],[0;20;0],0.1);
+figure(2), plot(t,xi)
+
+%% test
+clear, clc, close all
+
+% x->s
+a=BioProcess(3,2)
+
+a.Names={'x', 's', 'p'};
+
+% a.RateArray{1,1}=kineticModel('proportional',1);
+a.RateArray{1,2}=kineticModel('monod',0.5,0.5);
+% a.RateArray{2,1}=kineticModel('proportional',1);
+% a.RateArray{1,3}=kineticModel('inhibit',1,10);
+% a.RateArray{2,2}=kineticModel('haldane',0.7,0.6,5);
+
+% a.RateArray{2,2}=5;
 
 a.YieldMatrix = [1 0;-0.2 -0.3;0 1];
 xi_in=[0;10;0];
 
 
 a.stateSpace(1,[1;2;3],[0;10;0],0.1)
+
+[t,xi]=a.euler(0.01,[0 48],[0.1;1;0],[0;0;0],0.25);
+figure, plot(t,xi)
